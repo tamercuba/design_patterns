08620456899c5c098bfa757518802c4e2d22b65a
tamer.cuba <tamercuba@gmail.com>
2020-06-30 17:42:40 -0300

    initial builder implementation added

---
+++ builder_pattern/implementation.py
@@ -0,0 +1,99 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Product:
+    def __init__(self) -> None:
+        self.parts = list()
+
+    def add(self, value: int) -> None:
+        self.parts.append(value)
+
+    def __str__(self) -> str:
+        return f'Product parts: {str(self.parts)}'
+
+
+class AbstractBuilder(ABC):
+    @abstractmethod
+    def reset(self) -> None:
+        raise NotImplementedError
+
+    @property
+    def product(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def produce_one(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def produce_two(self) -> None:
+        raise NotImplementedError
+
+
+class Builder(AbstractBuilder):
+    def __init__(self) -> None:
+        self.reset()
+
+    @property
+    def product(self) -> Any:
+        """
+        This property is similar to "getResult" method, which the most
+        commonly used method name to this operation
+        """
+        product = self._product
+        self.reset()
+
+        return product
+
+    @product.setter
+    def product(self, product) -> None:
+        self._product = product
+
+    def reset(self) -> None:
+        self.product = Product()
+
+    def produce_one(self) -> None:
+        self._product.add(1)
+
+    def produce_two(self) -> None:
+        self._product.add(2)
+
+
+class Director:
+    def __init__(self):
+        self._builder = None
+
+    @property
+    def builder(self) -> Builder:
+        return self._builder
+
+    @builder.setter
+    def builder(self, builder: Builder) -> None:
+        self._builder = builder
+
+    def build_simple(self) -> None:
+        self.builder.produce_one()
+        self.builder.produce_two()
+
+    def build_repeated(self) -> None:
+        self.builder.produce_one()
+        self.builder.produce_two()
+        self.builder.produce_one()
+        self.builder.produce_two()
+
+    def get_result(self) -> Product:
+        return self._builder.product
+
+
+if __name__ == "__main__":
+    director = Director()
+    director.builder = Builder()
+
+    print('Build simple case: ')
+    director.build_simple()
+    print(director.get_result())
+
+    print('Build repeated case: ')
+    director.build_repeated()
+    print(director.get_result())
